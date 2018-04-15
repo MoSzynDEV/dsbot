850aca21ba35729282bcf95e371c20d4c1702507
varden4k <varden4k@gmail.com>
2018-04-15 14:26:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,10 +21,8 @@ client.on('message', async message => {
                 message.channel.send('Cena **mety** to **'+cennik.cennik.meta+'**');
                 } else if (args[1] == 'koka') {
                 message.channel.send('Cena **koki** to **'+cennik.cennik.koka+'**');
-                }
                 } else if (args[1] == 'hera') {
                 message.channel.send('Cena **hery** to **'+cennik.cennik.hera+'**');
-                }
                 } else if (args[1] == 'amfa') {
                 message.channel.send('Cena **amfy** to **'+cennik.cennik.amfa+'**');
                 }
