50d2df010cc06c4d48584f0e8f78978409668f44
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-05-09 17:01:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,7 +22,8 @@ client.on('message', async message => {
         let args = message.content.toLowerCase().split(/ +/);
         switch (args[0]) {
             case '/cena':
-                if (args[1] == 'walton' || message.channel.id == 443788782395981835 ) {
+                if (args[1] == 'walton' ) {
+		message.channel.id == ) 443788782395981835}
                 message.channel.send('✅ Cena **Waltona** to **$'+cennik.cennik.Walton+'**');
                 } else if (args[1] == 'waltonc') {
                 message.channel.send('✅ Cena **Waltona Rat-Loadera** to **$'+cennik.cennik.WaltonC+'**');
