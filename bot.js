6a49502adbe6325d239fe9b18ec95be59f72d135
MoSzynDEV <38393607+MoSzynDEV@users.noreply.github.com>
2018-05-09 17:24:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const cennik = require("./cennik.js");
 
 
 client.on('ready', () => {
-    console.log(' ZŁOM BUILD 0.2 ')
+    console.log(' ZŁOM BUILD 0.3 ')
     client.user.setUsername("Stanisław");
     client.user.setStatus('dnd');
     client.user.setActivity('się mądrych ludzi', {type: 'LISTENING'});
